47576cc1a6b29200f0d276168ab1116b0d35afb7
Ian Ramos <ianramosc@gmail.com>
2016-11-19 11:50:19 -0300

    Changing version

--- megasena.js
+++ megasena.js
@@ -4,22 +4,33 @@ var generateRandom = function generateRandom(_) {
   return ~~(Math.random() * 60) + 1;
 };
 
+var pad = function pad(num) {
+  return num < 10 ? Number('0' + num) : num;
+};
+
 var generateMegaSenaResult = function generateMegaSenaResult() {
   var num = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 6;
+  var returnAsString = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
+
+  if (isNaN(num)) {
+    throw new TypeError('The specified argument is not a number');
+  }
 
   var result = [];
 
   while (result.length < num) {
-    var number = generateRandom();
+    var number = pad(generateRandom());
 
     if (result.indexOf(number) < 0) {
       result.push(number);
     }
   }
 
-  return result.sort(function (a, b) {
+  result.sort(function (a, b) {
     return a - b;
-  }).join(', ');
+  });
+
+  return returnAsString ? result.join(', ') : result;
 };
 
 module.exports = generateMegaSenaResult;
